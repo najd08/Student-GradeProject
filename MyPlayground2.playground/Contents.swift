1c43a43959933539d27d25d2aebeb35c2f2515a0
najd08 <120324320+najd08@users.noreply.github.com>
2024-08-01 20:26:32 +0300

    Add Xcode Playground file

---
+++ MyPlayground2.playground/Contents.swift
@@ -0,0 +1,16 @@
+
+var studentMark = 64
+
+if(studentMark >= 90){
+    print("Excellent")
+} else if(studentMark >= 80){
+    print("Very Good")
+} else if(studentMark >= 70){
+    print("Good")
+} else if(studentMark >= 60){
+    print("Pass")
+}else {
+    print("Fail")
+}
+
+
